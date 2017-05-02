99e0935373a903612193c29334763f522e9f9b3a
DuanWenjieImech <duanwenjie@yosemi.cc>
2017-05-02 16:37:06 +0800

    Add files via upload

--- SinglePVPResultAnalysis.py
+++ SinglePVPResultAnalysis.py
@@ -1,4 +1,11 @@
 import json
+
+def PrintListOneByOne(tempList):
+    for i in range(len(tempList)):
+        print(tempList[i])
+        input()
+
+#Read PvP Data File
 jsonfile = open("part-00000.json", encoding='utf-8')
 datafile = jsonfile.readlines()
 jsonfile.close()
@@ -43,8 +50,61 @@ for i in range(len(WinRateArray)):
     tempWinRate = float(WinRateArray[i]["PvPResult"][0]) / float( WinRateArray[i]["PvPResult"][0] + WinRateArray[i]["PvPResult"][1] )
     WinRateArray[i]["PvPResult"][2] = str(float(tempWinRate)*100) + "%"
     
-print(WinRateArray)
+#PrintListOneByOne(WinRateArray)
 
+#Read PvP AI File
+jsonfile = open("part-00001.json", encoding='utf-8')
+datafile = jsonfile.readlines()
+jsonfile.close()
 
-    
+AIDataArray=[]
+for line in datafile:
+    tempresults = json.loads(line)
+    AIDataArray.append(tempresults)
 
+#Match AI with userId
+jsonfile = open("part-00002.json", encoding='utf-8')
+datafile = jsonfile.readlines()
+jsonfile.close()
+
+TeamDataArray=[]
+for line in datafile:
+    tempresults = json.loads(line)
+    TeamDataArray.append(tempresults)
+
+UserAIArray=[]
+for i in range(len(UserIDArray)):
+    AIListGroup=[];
+    tempUserAI={"UserId":UserIDArray[i],"AIList":AIListGroup,"PvPResult":WinRateArray[i]["PvPResult"]}
+    UserAIArray.append(tempUserAI)
+
+for i in range(len(AIDataArray)):
+    tempTeamId = AIDataArray[i]["team"]["objectId"]
+    tempAIUserId=""
+    for j in range(len(TeamDataArray)):
+        if (TeamDataArray[j]["objectId"] == tempTeamId):
+            tempAIUserId = TeamDataArray[j]["player"]["objectId"]
+    #PrintListOneByOne(tempAIUserId)
+    if tempAIUserId in UserIDArray:
+        tempUserIter = UserIDArray.index(tempAIUserId)
+        UserAIArray[tempUserIter]["AIList"].append(AIDataArray[i])
+
+#PrintListOneByOne(UserAIArray)
+
+#Sort AI with Win Rate
+RealUserAIArray=[]
+for i in range(len(UserAIArray)):
+    if ( len(UserAIArray[i]["AIList"]) != 0 ):
+        RealUserAIArray.append(UserAIArray[i])
+
+RealUserAIArray.sort(key=lambda d: float(d["PvPResult"][2].split("%")[0]),reverse=True)
+
+#PrintListOneByOne(RealUserAIArray)
+
+#Remove data which has less than 5 matches
+RealUsefulUserAIArray=[]
+for i in range(len(RealUserAIArray)):
+    if ( RealUserAIArray[i]["PvPResult"][0] + RealUserAIArray[i]["PvPResult"][1] >= 5 ):
+       RealUsefulUserAIArray.append(RealUserAIArray[i]);
+
+#PrintListOneByOne(RealUsefulUserAIArray)
